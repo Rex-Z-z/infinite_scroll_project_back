d0145f0fca72fcac8c0cc79c5d8f5d9080cc3472
Rex <195429265+Rex-Z-z@users.noreply.github.com>
2026-02-02 19:36:40 +0700

    Initial commit

---
+++ src/main/java/org/example/infinite_scroll_project_back/InfiniteScrollProjectBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.infinite_scroll_project_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfiniteScrollProjectBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfiniteScrollProjectBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/infinite_scroll_project_back/InfiniteScrollProjectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.infinite_scroll_project_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfiniteScrollProjectBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
