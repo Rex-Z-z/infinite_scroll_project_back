7f896c63642b5dd5dae833ab7e1e33bce0d87a76
Rex <195429265+Rex-Z-z@users.noreply.github.com>
2026-02-06 20:56:25 +0700

    Fix user register

--- src/main/java/org/example/infinite_scroll_project_back/modules/user/model/User.java
+++ src/main/java/org/example/infinite_scroll_project_back/modules/user/model/User.java
@@ -17,9 +17,6 @@ public class User implements UserDetails {
     public Collection<? extends GrantedAuthority> getAuthorities() { return List.of(); }
 
     @Override
-    public String getUsername() { return email; }
-
-    @Override
     public boolean isAccountNonExpired() { return true; }
 
     @Override
